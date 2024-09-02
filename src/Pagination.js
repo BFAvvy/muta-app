f6cb084f53c6675191d5b7d1d04c4c26fabc9083
BFAvvy <bayron.fontalvo@avvy.co>
2024-09-02 00:49:59 -0500

    Button

--- src/Pagination.js
+++ src/Pagination.js
@@ -6,17 +6,17 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
       <button
         onClick={() => onPageChange(currentPage - 1)}
         disabled={currentPage === 1}
-        className="px-4 py-2 bg-black text-green-400 rounded border border-green-400 disabled:bg-gray-800 disabled:text-gray-500 disabled:border-gray-500 disabled:cursor-not-allowed transition duration-300 ease-in-out hover:bg-green-400 hover:text-black"
+        className="px-4 py-2 bg-black text-green-400 rounded-full border border-green-400 disabled:bg-gray-800 disabled:text-gray-500 disabled:border-gray-500 disabled:cursor-not-allowed transition duration-300 ease-in-out transform hover:scale-105 hover:bg-green-400 hover:text-black"
       >
         Previous
       </button>
-      <span className="px-4 py-2 bg-black bg-opacity-80 text-green-400 rounded border border-green-400">
+      <span className="px-4 py-2 bg-black bg-opacity-80 text-green-400 rounded-full border border-green-400">
         Page {currentPage} of {totalPages}
       </span>
       <button
         onClick={() => onPageChange(currentPage + 1)}
         disabled={currentPage === totalPages}
-        className="px-4 py-2 bg-black text-green-400 rounded border border-green-400 disabled:bg-gray-800 disabled:text-gray-500 disabled:border-gray-500 disabled:cursor-not-allowed transition duration-300 ease-in-out hover:bg-green-400 hover:text-black"
+        className="px-4 py-2 bg-black text-green-400 rounded-full border border-green-400 disabled:bg-gray-800 disabled:text-gray-500 disabled:border-gray-500 disabled:cursor-not-allowed transition duration-300 ease-in-out transform hover:scale-105 hover:bg-green-400 hover:text-black"
       >
         Next
       </button>
